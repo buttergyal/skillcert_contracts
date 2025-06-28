b182e02cba86401eeab3302b77332dcddd03d1e7
hoossayn <abdulhakeemhusseni@gmail.com>
2025-06-28 09:29:55 +0100

    Remove  has_access function and add revoke_access function placeholder

--- contracts/course/course_access/src/functions/has_access.rs
+++ contracts/course/course_access/src/functions/has_access.rs
@@ -1,9 +1,1 @@
-use soroban_sdk::{Env, Address, String};
-use crate::schema::DataKey;
 
-/// Check if a user has access to a specific course
-
-pub fn course_access_has_access(env: Env, course_id: String, user: Address) -> bool {
-    let key = DataKey::CourseAccess(course_id, user);
-    env.storage().persistent().has(&key)
-}
