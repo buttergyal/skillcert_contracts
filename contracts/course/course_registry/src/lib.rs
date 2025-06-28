b3647339392474873bfdf44e471aa7dee9026de6
Hakeem Kazeem <hakymulla@gmail.com>
2025-06-28 09:36:09 +0100

    removed impl CourseRegistry

--- contracts/course/course_registry/src/lib.rs
+++ contracts/course/course_registry/src/lib.rs
@@ -1,17 +1,7 @@
-use soroban_sdk::{contract, contractimpl, Env, Address, String};
+use soroban_sdk::{contract};
 
 mod functions;
 mod schema;
 
-pub use functions::*;
-pub use schema::*;
-
 #[contract]
 pub struct CourseRegistry;
-
-#[contractimpl]
-impl CourseRegistry {
-    pub fn add_module(env: Env, course_id: String, position: i32, title: String) -> CourseModule {
-        course_registry_add_module(env, course_id, position, title)
-    }
-}
