6a97fea6baa0aa33d38fb2ccd773bf94edc8ef49
blurbeast <leumasre@gmail.com>
2025-07-01 21:09:33 +0100

    created a private fn to help generate course id

--- contracts/course/course_registry/src/functions/create_course.rs
+++ contracts/course/course_registry/src/functions/create_course.rs
@@ -1,16 +1,21 @@
 
 
-use soroban_sdk::{symbol_short, Address, Env, String, Symbol};
-use crate::schema::Course;
+
+use soroban_sdk::{symbol_short, Address, Env, String, Symbol, U256};
+use crate::schema::{Course, CourseId};
 
 const COURSE_KEY: Symbol = symbol_short!("course");
+const COURSE_ID_KEY: Symbol = symbol_short!("course_id");
 
 pub fn course_registry_create_course(env: Env, title: String, description: String) {
 
     // generate a course id
-    let timestamp:u64 = env.ledger().timestamp();
-    let caller: Address = env.invoker();
-    let course_id:String = format!("{}-{}", title, timestamp);
+    // use the title to generate the id
+    let course_id: String = String::from_bytes(&env, &generate_course_id(env.clone()).to_be_bytes());
+    // let caller: Address = Address::from_string("strkey");
+    let caller: Address = env.current_contract_address();
+
+    caller.require_auth();
 
     let storage_key: (Symbol, String) = (COURSE_KEY, course_id.clone());
 
@@ -19,8 +24,8 @@ pub fn course_registry_create_course(env: Env, title: String, description: Strin
     }
 
     // create a new course
-    let new_course = Course {
-        id: course_id,
+    let new_course: Course = Course {
+        id: course_id.clone(),
         title: title,
         description: description,
         creator: caller,
@@ -29,4 +34,22 @@ pub fn course_registry_create_course(env: Env, title: String, description: Strin
 
     // save to the storage
     env.storage().persistent().set(&storage_key, &new_course);
+}
+
+fn generate_course_id(env: Env) -> u128 {
+
+    let optional_course_id: Option<CourseId> = env.storage().persistent().get(&COURSE_ID_KEY);
+
+    let mut course_id: CourseId = optional_course_id.unwrap_or_else(|| CourseId {
+        id: String::from_str(&env, "course_id"),
+        count: 0,
+    });
+
+    let current_id = course_id.count;
+
+    course_id.count += 1;
+
+    env.storage().persistent().set(&COURSE_ID_KEY, &course_id);
+
+    current_id
 }
--- contracts/course/course_registry/src/schema.rs
+++ contracts/course/course_registry/src/schema.rs
@@ -1,12 +1,20 @@
 
-use soroban_sdk::Address;
+use soroban_sdk::{Address, String, contracttype};
 
+#[derive(Clone,)]
 #[contracttype]
-#[derive(Clone, Drop)]
 pub struct Course {
     pub id: String,
     pub title: String,
     pub description: String,
     pub creator: Address,
     pub published: bool,
+}
+
+
+#[derive(Clone,)]
+#[contracttype]
+pub  struct CourseId {
+    pub id: String,
+    pub count: u128,
 }
