a64a4635d59dc59f7e1e4dd865d89abfd84cefa1
Ikem Peter <ikempeter2020@gmail.com>
2025-08-13 09:08:54 +0100

    fix err

--- contracts/user_management/src/functions/is_admin.rs
+++ contracts/user_management/src/functions/is_admin.rs
@@ -3,5 +3,5 @@ use crate::schema::DataKey;
 
 /// Returns true if the given address has admin flag set in storage.
 pub fn is_admin(env: Env, who: Address) -> bool {
-    env.storage().get(&DataKey::Admin(who)).unwrap_or(false)
+    env.storage().persistent().get(&DataKey::Admin(who)).unwrap_or(false)
 }
