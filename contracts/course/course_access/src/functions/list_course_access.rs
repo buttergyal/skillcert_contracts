7341bdbb67ae499335be4352eb7d63da0c10e03a
pfed-prog <fedorovn19@gmail.com>
2025-09-15 19:10:47 +0200

    uncommented test code and fixed the test - it passes

--- contracts/course/course_access/src/functions/list_course_access.rs
+++ contracts/course/course_access/src/functions/list_course_access.rs
@@ -28,38 +28,42 @@ pub fn course_access_list_course_access(env: Env, course_id: String) -> CourseUs
     })
 }
 
-// #[cfg(test)]
-// mod test {
-//     use soroban_sdk::{symbol_short, testutils::Address as _, vec, Address, Env, String, Symbol};
+#[cfg(test)]
+mod test {
+    use crate::schema::{DataKey};
+    use soroban_sdk::{testutils::Address as _, vec, Address, Env, String};
 
-//     use crate::{course_access_list_course_access, CourseAccessContract, CourseUsers};
+    use crate::{course_access_list_course_access, CourseAccessContract, CourseUsers};
 
-//     const COURSES_KEY: Symbol = symbol_short!("courses");
+    #[test]
+    fn test() {
+        let env: Env = Env::default();
 
-//     #[test]
-//     fn test() {
-//         let env: Env = Env::default();
+        let contract_id: Address = env.register(CourseAccessContract, {});
 
-//         let contract_id: Address = env.register(CourseAccessContract, {});
+        let course_id: String = String::from_str(&env, "test_course_123");
 
-//         let course_id: String = String::from_str(&env, "test_course_123");
-//         let user1: Address = Address::generate(&env);
-//         let user2: Address = Address::generate(&env);
+        let key: DataKey = DataKey::CourseUsers(course_id.clone());
 
-//         let addresses: soroban_sdk::Vec<Address> = vec![&env, user1, user2];
+        let user1: Address = Address::generate(&env);
+        let user2: Address = Address::generate(&env);
 
-//         let course_users: CourseUsers = CourseUsers {
-//             users: addresses,
-//             course: course_id.clone(),
-//         };
+        let addresses: soroban_sdk::Vec<Address> = vec![&env, user1, user2];
 
-//         // Set up initial course data and perform test within contract context
-//         env.clone().as_contract(&contract_id, || {
-//             env.storage()
-//                 .persistent()
-//                 .set(&(COURSES_KEY, course_id.clone()), &course_users);
-//             let result: CourseUsers = course_access_list_course_access(env, course_id.clone());
-//             assert_eq!(result, course_users);
-//         });
-//     }
-// }
+        let course_users: CourseUsers = CourseUsers {
+            users: addresses,
+            course: course_id.clone(),
+        };
+
+        // Set up initial course data and perform test within contract context
+        env.clone().as_contract(&contract_id, || {
+            env.storage()
+                .persistent()
+                .set(&(key), &course_users);
+
+            let result: CourseUsers = course_access_list_course_access(env, course_id.clone());
+
+            assert_eq!(result, course_users);
+        });
+    }
+}
