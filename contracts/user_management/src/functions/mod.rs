d57950cc8dac18e03d937465308eccc8512d4899
emarc99 <emmanueladedejiaroso@gmail.com>
2025-09-19 03:02:04 +0100

    chore: expose function externally

--- contracts/user_management/src/functions/mod.rs
+++ contracts/user_management/src/functions/mod.rs
@@ -4,6 +4,7 @@
 pub mod admin_management;
 pub mod create_user_profile;
 pub mod delete_user;
+pub mod edit_user_profile;
 pub mod get_user_by_id;
 pub mod is_admin;
 pub mod list_all_registered_users;
